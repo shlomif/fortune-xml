d713923c99f11c8ecfaac554143f9f21fcb95b8e
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2011-04-22 12:23:31 +0000

    Clarified licence and POD.

--- XML-Grammar-Fortune/module/lib/XML/Grammar/Fortune.pm
+++ XML-Grammar-Fortune/module/lib/XML/Grammar/Fortune.pm
@@ -247,22 +247,24 @@ sub run
     return;
 }
 
+1; # End of XML::Grammar::Fortune
+
+__END__
+
 =head2 open
 
-Meant to settle Pod::Coverage - introduced by Fatal. Ignore.
+This function is introduced by Fatal. It is mentioned here, in order to settle 
+L<Pod::Coverage> . Ignore.
 
 =head1 AUTHOR
 
-Shlomi Fish, C<< <shlomif at iglu.org.il> >>
+Shlomi Fish, L<http://www.shlomifish.org/> .
 
 =head1 BUGS
 
 Please report any bugs or feature requests to C<bug-xml-grammar-fortune at rt.cpan.org>, or through
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=XML-Grammar-Fortune>.  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
-
-
-
 
 =head1 SUPPORT
 
@@ -296,13 +298,33 @@ L<http://search.cpan.org/dist/XML-Grammar-Fortune>
 
 =head1 ACKNOWLEDGEMENTS
 
-
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2008 Shlomi Fish, all rights reserved.
+Copyright 2008 by Shlomi Fish
 
-This program is released under the following license: MIT X11.
+This program is distributed under the MIT (X11) License:
+L<http://www.opensource.org/licenses/mit-license.php>
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
 
 =cut
 
-1; # End of XML::Grammar::Fortune
