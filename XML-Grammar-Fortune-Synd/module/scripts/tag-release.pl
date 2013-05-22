5a9d44385140ade9aa3513bca3949c9c322003bd
Shlomi Fish <shlomif@shlomifish.org>
2013-05-22 12:00:17 +0300

    Fix scripts/tag-release.pl

--- XML-Grammar-Fortune-Synd/module/scripts/tag-release.pl
+++ XML-Grammar-Fortune-Synd/module/scripts/tag-release.pl
@@ -16,13 +16,10 @@ if (!defined ($version))
     die "Version is undefined!";
 }
 
-my $mini_repos_base = 'https://svn.berlios.de/svnroot/repos/web-cpan/XML-Grammar-Fortune';
-
 my @cmd = (
-    "svn", "copy", "-m",
+    "hg", "tag", "-m",
     "Tagging the XML-Grammar-Fortune-Synd release as $version",
-    "$mini_repos_base/trunk",
-    "$mini_repos_base/tags/releases/XML-Grammar-Fortune-Synd/cpan/$version",
+    "releases/XML-Grammar-Fortune-Synd/cpan/$version",
 );
 
 print join(" ", map { /\s/ ? qq{"$_"} : $_ } @cmd), "\n";
